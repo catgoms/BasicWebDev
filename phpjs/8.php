ced347df7969d8fbed3bbaabdf0f1f4a52b01870
Soo Bin Lee <mariddo3sb@gmail.com>
2023-02-22 00:47:21 +1000

    while loop phpjs

---
+++ phpjs/8.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+ 
+<body>
+    <h1>JavaScript</h1>
+    <ul>
+    <script charset="utf-8">
+        i = 0;
+        while (i < 10) {
+            document.write("<li>hello world</li>");
+            i++;
+        }
+    </script>
+    </ul>
+    
+    <h1>php</h1>
+    <?php
+        $i = 0;
+        while ($i < 10) {
+            echo "hello world<br />";
+            $i = $i + 1;
+        }
+    ?>
+</body>
+</html>
