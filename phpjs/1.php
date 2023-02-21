1e433e2d861157ce9b1973ecfeb7aab53f56a892
Soo Bin Lee <soobin.lee@qut>
2023-02-21 23:45:05 +1000

    added php code

--- phpjs/1.php
+++ phpjs/1.php
@@ -5,9 +5,13 @@
 </head>
  
 <body>
+    <h1>php</h1>
     <?php
-        echo "Hello World";
-        
+        echo 10+10;
     ?>
+    <h1>JavaScript</h1>
+    <script>
+        document.write("Hello World");
+    </script>
 </body>
 </html>
