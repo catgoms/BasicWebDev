1910c746c9fdeb6afce62956dd25a4adbb2397ae
Soo Bin Lee <mariddo3sb@gmail.com>
2023-02-28 19:57:25 +1000

    Updated UI design using Bootstrap

--- index.php
+++ index.php
@@ -15,45 +15,56 @@
 </head>
  
 <body id="target">
-    <header>
-        <img src="https://media.istockphoto.com/id/153238484/photo/polar-bear-cub-ursus-maritimus-3-months-old-standing.jpg?s=612x612&w=0&k=20&c=nEvMlp6lUHOWuo1NzuutlHH-U-IEdpH23K5CHK0Y1aQ=" alt="bear" />
-        <h1><a href="http://localhost/basicwebdev/index.php">JavaScript</a></h1>
-    </header>
-    
-    <nav>
-        <ol>
-            <?php
-            while($row = mysqli_fetch_assoc($result)) {
-                echo '<li><a href ="http://localhost/basicwebdev/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
-            }       
-            ?>
-        </ol>
-    </nav>
+    <div class="container">
+        <header class="jumbotron text-center">
+            <img src="https://media.istockphoto.com/id/153238484/photo/polar-bear-cub-ursus-maritimus-3-months-old-standing.jpg?s=612x612&w=0&k=20&c=nEvMlp6lUHOWuo1NzuutlHH-U-IEdpH23K5CHK0Y1aQ=" alt="bear" class="img-circle" id="logo" />
+            <h1><a href="http://localhost/basicwebdev/index.php">JavaScript</a></h1>
+        </header>
+        
+        <div class="row">
+            <nav class="col-md-3">
+                <ol class="nav nav-pills nav-stacked">
+                    <?php
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo '<li><a href ="http://localhost/basicwebdev/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+                    }       
+                    ?>
+                </ol>
+            </nav>
 
-    <div id="control">
-        <input type="button" value="white" id="white_btn" />
-        <input type="button" value="black" id="black_btn" />
-        <a href="http://localhost/basicwebdev/write.php">Write</a>
+            <div class="col-md-9">
+            <article>
+                <?php
+                if (empty($_GET['id']) === false) {
+                    $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author=user.id WHERE topic.id=".$_GET['id'];
+                    $result = mysqli_query($conn, $sql);
+                    $row = mysqli_fetch_assoc($result);
+                    echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+                    echo '<p>'.htmlspecialchars($row['name']).'</p>';
+                    echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ol><ul><li><p>');
+                }
+                ?>
+            </article>
+            <hr>
+            <div id="control">
+                <div class="btn-group" role="group" aria-label="...">
+                    <input type="button" value="White" id="white_btn" class="btn btn-default btn-lg" />
+                    <input type="button" value="Black" id="black_btn" class="btn btn-default btn-lg" />
+                </div>
+                <a href="http://localhost/basicwebdev/write.php" class="btn btn-success btn-lg">Write</a>
+            </div>
+            </div>
+        </div>
     </div>
+    
 
-    <article>
-        <?php
-        if (empty($_GET['id']) === false) {
-            $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author=user.id WHERE topic.id=".$_GET['id'];
-            $result = mysqli_query($conn, $sql);
-            $row = mysqli_fetch_assoc($result);
-            echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-            echo '<p>'.htmlspecialchars($row['name']).'</p>';
-            echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ol><ul><li><p>');
-        }
-        ?>
-    </article>
+    
 
     <script src="http://localhost/basicwebdev/script.js"></script>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="http://localhost/basciwebdev/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+    <script src="http://localhost/basicwebdev/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
--- write.php
+++ write.php
@@ -8,46 +8,67 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="http://localhost/basicwebdev/style.css">
+    <link href="http://localhost/basicwebdev/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
  
 <body id="target">
-    <header>
-        <img src="https://media.istockphoto.com/id/153238484/photo/polar-bear-cub-ursus-maritimus-3-months-old-standing.jpg?s=612x612&w=0&k=20&c=nEvMlp6lUHOWuo1NzuutlHH-U-IEdpH23K5CHK0Y1aQ=" alt="bear" />
-        <h1><a href="http://localhost/basicwebdev/index.php">JavaScript</a></h1>
-    </header>
-    
-    <nav>
-        <ol>
-            <?php
-            while($row = mysqli_fetch_assoc($result)) {
-                echo '<li><a href ="http://localhost/basicwebdev/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
-            }       
-            ?>
-        </ol>
-    </nav>
+    <div class="container">
+        <header class="jumbotron text-center">
+            <img src="https://media.istockphoto.com/id/153238484/photo/polar-bear-cub-ursus-maritimus-3-months-old-standing.jpg?s=612x612&w=0&k=20&c=nEvMlp6lUHOWuo1NzuutlHH-U-IEdpH23K5CHK0Y1aQ=" alt="bear" class="img-circle" id="logo" />
+            <h1><a href="http://localhost/basicwebdev/index.php">JavaScript</a></h1>
+        </header>
+        
+        <div class="row">
+            <nav class="col-md-3">
+            <ol class="nav nav-pills nav-stacked">
+                <?php
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo '<li><a href ="http://localhost/basicwebdev/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+                }       
+                ?>
+            </ol>
+            </nav>
 
-    <div id="control">
-        <input type="button" value="white" id="white_btn" />
-        <input type="button" value="black" id="black_btn" />
-        <a href="http://localhost/basicwebdev/write.php">Write</a>
+            <div class="col-md-9">
+            <article>
+            <form action="process.php" method="POST">
+
+                <div class="form-group">
+                    <label for="form-title">Title</label>
+                    <input type="text" class="form-control" name="title" id="form-title" placeholder="Enter title">
+                </div>
+                <div class="form-group">
+                    <label for="form-author">Author</label>
+                    <input type="text" class="form-control" name="author" id="form-author" placeholder="Enter author name">
+                </div>
+                <div class="form-group">
+                    <label for="form-description">Description</label>
+                    <textarea name="description" id="form_description" class="form-control" placeholder="Enter description" rows="10"></textarea>
+                </div>
+                <input type="submit" name="name" class="btn btn-default btn-lg">
+            </form>
+            </article>
+            <hr>
+            <div id="control">
+                <div class="btn-group" role="group" aria-label="...">
+                    <input type="button" value="White" id="white_btn" class="btn btn-default btn-lg" />
+                    <input type="button" value="Black" id="black_btn" class="btn btn-default btn-lg" />
+                </div>
+                <a href="http://localhost/basicwebdev/write.php" class="btn btn-success btn-lg">Write</a>
+            </div>
+            </div>
+        </div>
     </div>
-
-    <article>
-        <form action="process.php" method="POST">
-            <p>
-                Title: <input type="text" name="title">
-            </p>
-            <p>
-                Author: <input type="text" name="author">
-            </p>
-            <p>
-                Text: <textarea name="description"></textarea>
-            </p>
-            <input type="submit">
-        </form>
-    </article>
+    
 
     <script src="http://localhost/basicwebdev/script.js"></script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://localhost/basicwebdev/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
