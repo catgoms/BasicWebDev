01d4338f3bfd5150e314080af2db664d116429f4
Soo Bin Lee <mariddo3sb@gmail.com>
2023-02-28 18:53:29 +1000

    simple lib & config created

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+    "host"=>"localhost:3307",
+    "duser"=>"root",
+    "dpw"=>"admin1",
+    "dname"=>"opentutorials"
+);
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    $conn = mysqli_connect("localhost:3307", "root", "admin1");
-    mysqli_select_db($conn, "opentutorials");
+    require("config/config.php");
+    require("lib/db.php");
+    $conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
     $result = mysqli_query($conn, "SELECT * FROM topic");
 ?>
 <!DOCTYPE html>
